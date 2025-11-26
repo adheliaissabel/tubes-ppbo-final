e34cbbfb56677b53a4cf61b0d0699bd6a9a88ac9
Blackpa77 <h1101241029@student.untan.ac.id>
2025-11-26 16:04:02 +0700

    reset database

--- config/database.php
+++ config/database.php
@@ -12,11 +12,13 @@ class Database {
 
     public function __construct() {
         // LOGIKA CERDAS (Inovasi): 
+        // Cek apakah kode jalan di Railway (Environment Variables ada) atau di Localhost (XAMPP)
+        
         $this->host = getenv('MYSQLHOST') ? getenv('MYSQLHOST') : 'localhost';
         $this->port = getenv('MYSQLPORT') ? getenv('MYSQLPORT') : '3306';
         $this->db_name = getenv('MYSQLDATABASE') ? getenv('MYSQLDATABASE') : 'gudang_fashion'; // Samakan nama DB
         $this->username = getenv('MYSQLUSER') ? getenv('MYSQLUSER') : 'root';
-        $this->password = getenv('MYSQLPASSWORD') ? getenv('MYSQLPASSWORD') : ''; 
+        $this->password = getenv('MYSQLPASSWORD') ? getenv('MYSQLPASSWORD') : 'mkjw4004'; // Password lokal kamu
     }
 
     public function getConnection() {
