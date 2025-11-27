4851d9fcb448b176eaa7dfa49834baa6ff215277
adheliaissabel <h1101241001@student.untan.ac.id>
2025-11-27 12:21:10 +0700

    upd

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Cek apakah user sudah login
 if(!isset($_SESSION['user_id'])) {
     header("Location: ../index.php");
